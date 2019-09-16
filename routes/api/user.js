8ea6c3d92edfd4bda971052b6d7124f6beb4bd3c
jayesh kukreja <31185567+Jaykukreja@users.noreply.github.com>
2019-09-17 00:41:00 +0530

    Add files via upload

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,52 @@
 const express=require('express')
-const router = express.Router()
+const router = express.Router();
+const gravatar =require('gravatar');
+const becrypt = require('bcryptjs')
 
+//load user model
+const User = require('../../models/User')
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.get('/test',(req,res)=>res.json({msg:"it workss"}));
 
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post('/register',(req,res)=>{
+	User.findOne({email:req.body.email})
+		.then(user => {
+			if(user){
+				return res.status(400).json({email : 'email already exists'})
+			}
+			else {
+				const avatar =gravatar.url(req.body.email,{
+					s: '200', //size
+					r: 'pg', //rating
+					d: 'mm'	//default
+				})
+
+
+				const newUser = new User({
+					name : req.body.name,
+					email : req.body.email,
+					avatar,
+					name : req.body.password
+				});
+
+				bcrypt.genSalt(10,(err,salt)=> {
+					bcrypt.hash(newUser.password,salt,(err,hash)=>{
+						if (err) throw err;
+						newUser.password=hash;
+						newUser.save()
+						.then(user=>res.json(user))
+						.catch(err=> console.log(err))
+					})
+				})
+
+			}
+		})
+})
 module.exports=router;
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express =require('express');
+const mongoose = require("mongoose");
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 //const connectDB = require('./config/db');
+
+// DB 
+connectDB()
+const app = express();
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Define Routes
 const user=require('./routes/api/user');
 //const post=require('./routes/api/post');
 //const profile=require('./routes/api/profile');
 //const path = require('path');
 
-const app = express();
+
+
+
+
+
+
+
+
 
 app.get('/',(req,res)=> res.send('hello world'));
 
