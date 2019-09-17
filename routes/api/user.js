346d033aa29a6cb4794dff1663c130b7b53219ac
jayesh kukreja <31185567+Jaykukreja@users.noreply.github.com>
2019-09-17 23:42:29 +0530

    made changes in user.js
    completed registration and validation of user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,10 @@
 const express=require('express')
 const router = express.Router();
 const gravatar =require('gravatar');
-const becrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt =require("jsonwebtoken");
+const keys=require('../../config/keys')
+const passport =require('passport');
 
 //load user model
 const User = require('../../models/User')
@@ -33,7 +36,7 @@ router.post('/register',(req,res)=>{
 					name : req.body.name,
 					email : req.body.email,
 					avatar,
-					name : req.body.password
+					password : req.body.password
 				});
 
 				bcrypt.genSalt(10,(err,salt)=> {
@@ -49,4 +52,55 @@ router.post('/register',(req,res)=>{
 			}
 		})
 })
+
+// @route    POST api/users/login
+// @desc     login user / returning JWT token
+// @access   Public
+router.post('/login',(req,res)=>{
+	const email=req.body.email;
+	const password=req.body.password;
+
+	//find user by email
+	User.findOne({email})
+	.then(user => {
+		//check for user
+		if (!user){
+			return res.status(404).json({email:'user not found'});
+		}
+
+		//check password 
+		bcrypt.compare(password,user.password)
+		.then(isMatch =>{
+			if(isMatch){
+				//user matched
+
+
+				const payload={id:user.id,name:user.name,avatar: user.avatar}  //create jwt payload
+				//sign token
+				jwt.sign(payload,
+					keys.secretOrkey,
+					{expiresIn:3600}, 
+					(err,token)=>{
+						res.json({
+							success:true,
+							token:'Bearer' + token
+						})
+					});
+			}
+			else {
+				return res.status(400).json({password:'password incorrect'})
+			}
+		})
+	})
+});
+
+
+
+// @route    GET  api/users/current
+// @desc     return current user
+// @access   private
+router.get('/current',passport.authenticate('jwt',{session :false}),(req,res)=>{
+	res.json({msg:'success'})
+})
+
 module.exports=router;
