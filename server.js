e08fa987773e70629e467690ada33852a1ee316c
jayesh kukreja <31185567+Jaykukreja@users.noreply.github.com>
2019-09-24 17:16:58 +0530

    added login api with authentication

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const express =require('express');
 const mongoose = require("mongoose");
-const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 //const connectDB = require('./config/db');
 const passport =require('passport');
+const user=require('./routes/api/user');
+//intialize express
+const app = express();
 
 // DB 
-connectDB()
-const app = express();
+const db =require('./config/keys').mongoURI;
+console.log(db)
+
+
 
 //passport middleware
 app.use(passport.initialize());
 
 //passport config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+//DB config
+mongoose
+	.connect(db,{useNewUrlParser: true,useUnifiedTopoly:true})
+	.then(()=> console.log("mongo connected"))
+	.catch(err=> console.log(err))
+
+
 // Define Routes
-const user=require('./routes/api/user');
+
 //const post=require('./routes/api/post');
 //const profile=require('./routes/api/profile');
 //const path = require('path');
