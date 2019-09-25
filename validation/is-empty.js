3e1f9ef2943068567cc1488c692d2e1fa1f6919f
jayesh kukreja <31185567+Jaykukreja@users.noreply.github.com>
2019-09-25 21:17:44 +0530

    fixed minor bugs in validation

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,9 +1,6 @@
-const isEmpty = value => {
+const isEmpty = value => 
 		value === undefined ||
-		value === null 
-
-		// //checking whether object is empty or not using object.keys
-		// (typeof value === 'object' && object.keys(value).length === 0) || 
-		// (typeof value ==='string' && value.trim().length === 0);
-		}
-module.exports= isEmpty;
+		value === null ||
+		(typeof value === 'object' && Object.keys(value).length === 0) || 
+		(typeof value ==='string' && value.trim().length === 0)		
+module.exports = isEmpty;
--- validation/login.js
+++ validation/login.js
@@ -8,23 +8,17 @@ module.exports = function validateLogininput(data){
 	
 	data.email=!isEmpty(data.email) ? data.email : '';
 	data.password=!isEmpty(data.password) ? data.password : '';
-
-	
-
 	
 	if(!validator.isEmail(data.email)){
 		errors.email ='Email is invalid';
-
 	}
 
 	if(validator.isEmpty(data.email)){
 		errors.email ='Email field is required';
-
 	}
 
 	if(validator.isEmpty(data.password)){
 		errors.password ='Password field is required';
-
 	}
 
 	return {
